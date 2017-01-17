d37433165f615d01f44ebdad4a5bd054415496ce
Felix Alexis <felixalxis14@hotmail.com>
2017-01-17 21:51:21 +0000

    New Chnages

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //Write your function calls below this line
+setColor(2, 2, "blue");
+setColor(3, 2, "blue");
+setColor(4, 3, "blue");
+setColor(4, 4, "blue");
+setColor(5, 5, "purple");
+setColor(6, 6, "green");
+setColor(5, 6, "green");
+setColor(7, 5, "black");
+setColor(7, 4, "red");
+setColor(7, 3, "red");
+setColor(6, 2, "red");
+setColor(1, 5, "blue");
+
+
+
+
+
